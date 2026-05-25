1813379aa8a8845c3bcb1a6cbc31c2ec7863c950
Hajun Jeon <gkwns1231@naver.com>
2026-05-25 20:27:13 +0900

    각 페이지를 app에 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import CivilizationDetailPage from "./pages/CivilizationDetailPage";
+import CivilizationGuidePage from "./pages/CivilizationGuidePage";
+import CommunityGuidePage from "./pages/CommunityGuidePage";
+import HomePage from "./pages/HomePage";
+import PlacementToolPage from "./pages/PlacementToolPage";
+import WarGuidePage from "./pages/WarGuidePage";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/placement" element={<PlacementToolPage />} />
+        <Route path="/civilizations" element={<CivilizationGuidePage />} />
+        <Route
+          path="/civilizations/:civilizationId"
+          element={<CivilizationDetailPage />}
+        />
+        <Route path="/war" element={<WarGuidePage />} />
+        <Route path="/community" element={<CommunityGuidePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
