ff3a55e172642e940731ce3f4d8cfc036f063a07
Hajun Jeon <gkwns1231@naver.com>
2026-05-25 21:06:57 +0900

    상세 페이지에 해당 문명의 내용이 나오도록 수정

--- src/pages/CivilizationDetailPage.tsx
+++ src/pages/CivilizationDetailPage.tsx
@@ -1,5 +1,23 @@
+import { useParams } from "react-router-dom";
+import { civilizations } from "../data/civilizations";
+
 function CivilizationDetailPage() {
-  return <h1>문명 상세 공략</h1>;
+  const { civilizationId } = useParams();
+
+  const civilization = civilizations.find((item) => item.id === civilizationId);
+
+  if (!civilization) {
+    return <p>존재하지 않는 문명입니다.</p>;
+  }
+
+  return (
+    <section>
+      <h1>{civilization.name}</h1>
+      <p>지도자: {civilization.leader}</p>
+      <p>추천 승리: {civilization.recommendedVictory}</p>
+      <p>{civilization.summary}</p>
+    </section>
+  );
 }
 
 export default CivilizationDetailPage;
